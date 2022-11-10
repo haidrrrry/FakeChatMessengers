e224714f98e198b2cf11add2831a631e01ed3720
haidrrrry <fearlessquad0@gmail.com>
2022-11-10 04:57:25 -0800

    today work

---
+++ app/src/main/java/com/codecue/fakechatmessengers/activities/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.codecue.fakechatmessengers.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codecue.fakechatmessengers.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    private  lateinit var binding:ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
