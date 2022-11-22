f5c3a82a44ff3ed26d8dafeb1d8bf3775a8c1648
haidrrrry <fearlessquad0@gmail.com>
2022-11-22 16:30:41 +0500

    today work

--- app/src/main/java/com/codecue/fakechatmessengers/activities/HomeActivity.kt
+++ app/src/main/java/com/codecue/fakechatmessengers/activities/HomeActivity.kt
@@ -1,7 +1,11 @@
 package com.codecue.fakechatmessengers.activities
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.codecue.fakechatmessengers.activities.FakeChatActivities.ChatListActivity
 import com.codecue.fakechatmessengers.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -11,5 +15,26 @@ class HomeActivity : AppCompatActivity() {
         binding=ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setOnClickListners()
+
+    }
+
+    private fun setOnClickListners() {
+        binding.fakechathome.setOnClickListener{
+            val i=Intent(this,ChatListActivity::class.java)
+            startActivity(i)
+        }
+        binding.contactushome.setOnClickListener{
+            Toast.makeText(this, "Click on Contact Us!!", Toast.LENGTH_LONG).show()
+        }
+        binding.policyprivacyHome.setOnClickListener{
+            Toast.makeText(this, "Click on Policy Privacy Us!!", Toast.LENGTH_LONG).show()
+        }
+        binding.Sharebtnlinearlayout.setOnClickListener{
+            Toast.makeText(this, "Click on  Share Btn", Toast.LENGTH_LONG).show()
+        }
+        binding.getpremiumaccessBtn.setOnClickListener{
+            Toast.makeText(this, "Check Premium Version", Toast.LENGTH_LONG).show()
+        }
     }
 }
